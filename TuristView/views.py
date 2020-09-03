83b1eaadd3c46bd018e998de84b522cf63bc7680
Allison Espinoza <alleespi@espol.edu.ec>
2020-09-03 10:49:07 -0500

    admin

--- TuristView/views.py
+++ TuristView/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from TuristView.serializer import UserSerializer, GroupSerializer
+#from rest_framework import viewsets
+#from rest_framework import permissions
+#from TuristView.serializer import UserSerializer, GroupSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+'''class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
@@ -22,4 +22,4 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]'''
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TuristView.apps.TuristviewConfig',
-    'rest_framework',
+    #'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+#REST_FRAMEWORK = {
+#   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#    'PAGE_SIZE': 10
+#}
